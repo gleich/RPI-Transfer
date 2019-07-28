8d6fbb5834409ce33342ef081f43082928550ec3
Matthew Gleich <matthewgleich@gmail.com>
2019-07-27 22:58:24 -0400

    Fix main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     Runs the whole program
     :return: None
     """
-    os.chdir("cd ~/../../pi")
+    os.chdir("~/../../pi")
     with open("settings.json") as settings_json:
         settings = load(settings_json)
     drive = settings["drive-name"]
@@ -15,12 +15,8 @@ def main():
     if drive == "" or cards[0] == "":
         raise Exception("Please fill in settings.json")
     drives = os.listdir()
-    if drive not in drive:
-        raise Exception("Can't seem to find the drive: " + drive)
     file_paths = []
     for card in cards:
-        if card not in drive:
-            raise Exception("Can't seem to find the card: " + drive)
         card_content = os.listdir(card)
         for file in card_content:
             file_path = "/" + card + "/" + file
